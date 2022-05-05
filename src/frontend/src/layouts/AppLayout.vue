f204a757a92d2b81727b459359fbe3541cc66038
ibabushkin <ibabushkin@elewise.com>
2022-05-05 21:02:28 +0400

    Add default layout, add index view, add mock data for Index.vue

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div id="app">
+      <AppHeader />
+      <main class="content">
+        <IndexPage />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/layouts/AppHeader";
+import IndexPage from "../views/Index";
+
+export default {
+  name: "AppLayout",
+  components: { AppHeader, IndexPage },
+};
+</script>
+
+<style scoped></style>
