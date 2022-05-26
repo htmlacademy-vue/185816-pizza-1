364ba58055cb10389a5a4882e4c8bffcdfdc7236
ibabushkin <ibabushkin@elma-bpm.com>
2022-05-26 12:30:47 +0400

    add mutations file

---
+++ src/frontend/src/store/mutations.js
@@ -0,0 +1,5 @@
+export const SET_SIZE = "SET_SIZE";
+export const SET_DOUGH = "SET_DOUGH";
+export const SET_SAUCE = "SET_SAUCE";
+export const ADD_INGREDIENT = "ADD_INGREDIENT";
+export const REMOVE_INGREDIENT = "REMOVE_INGREDIENT";
